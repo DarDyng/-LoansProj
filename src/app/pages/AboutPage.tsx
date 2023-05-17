a1d9ab2c228386a2fe7b00a092ae95ff30be7463
JLW157 <alexbiletskyi17@gmail.com>
2023-05-18 00:52:36 +0300

    about page added

---
+++ src/app/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+    return <>
+        <h1>This program is Loan Portfolio.</h1>
+
+        <p>Why do you need this program? If you have several loans or payments from different banks, you can conveniently manage your loans in this program. The program localizes all loan data in one place and helps you get rid of all loans as quickly as possible.</p>
+
+        <p>On the main page, you can manage your personal loan portfolio. There you can add the name of the loan, the amount, the start date, and the interest rate at which you take it. The program will store this information for further analysis. These loans will automatically appear on the main page in the list of loans. Credit data can be saved and updated for future use.</p>
+
+        <p>In the statistics tab, information will appear that will help you pay off credits or loans faster. The program will analyze the entered data about loans and provide the user with an overview of their current debt status. Also, in this menu, the program calculates the minimum amount that should be paid this month so that the amount of debt does not increase in the next, taking into account all credits and loans.</p>
+    </>
+};
+
+export default AboutPage;
--- src/app/router/router-config.tsx
+++ src/app/router/router-config.tsx
@@ -5,14 +5,16 @@ import NotFoundPage from "../pages/NotFoundPage";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
 import { AuthContextErrorHandler, AuthProvider } from "../components/contexts/AuthContextErrorHandler";
+import AboutPage from "../pages/AboutPage";
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
             <Route index element={<MainPage />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="about" element={<AboutPage/>}></Route>
             <Route path="*" element={<NotFoundPage />} />
         </Route>
     )
